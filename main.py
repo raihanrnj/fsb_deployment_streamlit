a6472f38f6f8bf23ca0e394ba0b33d6527c8ce78
raihan nugroho Jauhari <raihanrnj@gmail.com>
2023-02-05 07:49:55 +0700

    push init

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ virginica = Image.open('assets/virginica.png')
 st.sidebar.title("Features")
 
 #Intializing
-parameter_list=['Sepal length (cm)','Sepal Width (cm)','Petal length (cm)','Petal Width (cm)']
+parameter_list=['sepal_length','sepal_width','petal_length','petal_width']
 parameter_input_values=[]
 parameter_default_values=['5.2','3.2','4.2','1.2']
 
